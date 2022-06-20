8847ed0b1e7b53cf7e510154aa2f448b34e59233
Kerriea-star <ojared511@gmail.com>
2022-06-20 18:54:55 +0300

    Learnt how to add commnts both single line and multiline

--- app/src/main/java/com/jsoftengineer/helloworld/KotlinBasics.kt
+++ app/src/main/java/com/jsoftengineer/helloworld/KotlinBasics.kt
@@ -1,6 +1,14 @@
 package com.jsoftengineer.helloworld
 
 fun main(){
+    // immutable variable
+    //TODO: Add new functionality
+
+    /*
+    This is a
+    multiline
+    comment
+     */
     val myName = "Jared"
     println("Hello " + myName)
 }
