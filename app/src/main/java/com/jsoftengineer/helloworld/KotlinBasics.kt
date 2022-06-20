c91e434c39b1aa317e590f513d35a7e98784d2de
Kerriea-star <ojared511@gmail.com>
2022-06-20 19:02:13 +0300

    Learnt different types of integers according to size they take in a memory

--- app/src/main/java/com/jsoftengineer/helloworld/KotlinBasics.kt
+++ app/src/main/java/com/jsoftengineer/helloworld/KotlinBasics.kt
@@ -9,6 +9,17 @@ fun main(){
     multiline
     comment
      */
+    // type string
     val myName = "Jared"
+    // type int
+    /*
+    Integer types: Byte (8 bit), short (16 bit),
+    Int (32 bit) and Long (64 bit)
+     */
+    var myAge = 20
+    val myByte: Byte = 13
+    val myShort: Short = 125
+    val myInt: Int = 123123123
+    val myLong: Long = 1203981230948712039
     println("Hello " + myName)
 }
