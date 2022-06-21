b58d6811f0470ffd62c37394bcaac41f0e2b4970
Kerriea-star <ojared511@gmail.com>
2022-06-21 11:34:01 +0300

    Leaned how to use arithmetic operators in kotlin

--- app/src/main/java/com/jsoftengineer/helloworld/KotlinBasics.kt
+++ app/src/main/java/com/jsoftengineer/helloworld/KotlinBasics.kt
@@ -53,4 +53,16 @@ fun main(){
     println("first character " + firstCharStr)
     println("last character " + lastCharStr)
     println("Hello " + myName)
+
+    // Ariyhmetic operators (+, -, *, /, %)
+    var result = 5+3
+    //result /= 2
+    //result *= 2
+    result = 15%2
+    println(result)
+    val a = 5.0
+    val b = 3.0
+    var resultDouble: Double
+    resultDouble = a / b
+    println(resultDouble)
 }
