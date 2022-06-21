4b5dcf57430350c927013af7c57fee768d322eb8
Kerriea-star <ojared511@gmail.com>
2022-06-21 09:50:25 +0300

    Learned Kotlin data types

--- app/src/main/java/com/jsoftengineer/helloworld/KotlinBasics.kt
+++ app/src/main/java/com/jsoftengineer/helloworld/KotlinBasics.kt
@@ -16,10 +16,41 @@ fun main(){
     Integer types: Byte (8 bit), short (16 bit),
     Int (32 bit) and Long (64 bit)
      */
+    // type inference finds out the type from the context
     var myAge = 20
     val myByte: Byte = 13
     val myShort: Short = 125
     val myInt: Int = 123123123
     val myLong: Long = 1203981230948712039
+    println(myAge)
+    println(myByte)
+    println(myShort)
+    println(myInt)
+    println(myLong)
+
+    // Floating point number Types: Float (32 bit), Double (64 bit)
+    val myFloat: Float = 13.37F
+    val myDouble: Double = 3.145926538979323846
+    println(myFloat)
+    println(myDouble)
+
+    //Boolean is a type that is used to represent logical values.
+    // It can have two possible values; true and false
+    var isSunny = true
+    isSunny = false
+
+    // character
+    val letterChar = 'A'
+    val digitChar = 'i'
+    println(digitChar)
+    println(letterChar)
+
+    // String is a collection of multiple char
+    val myString = "Hello world"
+    var firstCharStr = myString[0]
+    var lastCharStr = myString[myString.length - 1]
+
+    println("first character " + firstCharStr)
+    println("last character " + lastCharStr)
     println("Hello " + myName)
 }
